52abdaebf0f3c26affbdba17db7853e30fa5ee75
Arthi0420 <arthi20112003@gmail.com>
2025-01-28 13:07:26 +0530

    update url

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'https://taskify-client-delta.vercel.app/',
     methods: 'GET, PUT, POST, DELETE',
     credentials: true,
 }));
